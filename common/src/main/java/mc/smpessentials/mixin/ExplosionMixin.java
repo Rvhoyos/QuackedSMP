16b136c84294b9bab10854ef6eaf467f76354ea2
Raul <raultheta@gmail.com>
2026-03-09 00:20:10 -0400

    bug fix creeper explosion

--- common/src/main/java/mc/smpessentials/mixin/ExplosionMixin.java
+++ common/src/main/java/mc/smpessentials/mixin/ExplosionMixin.java
@@ -6,15 +6,15 @@ import net.minecraft.world.level.ServerExplosion;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ServerExplosion.class)
 public abstract class ExplosionMixin {
     @Inject(method = "explode", at = @At("HEAD"), cancellable = true)
-    private void onExplode(CallbackInfo ci) {
+    private void onExplode(CallbackInfoReturnable<Integer> cir) {
         Explosion explosion = (Explosion) (Object) this;
         if (!ClaimProtection.onExplosionPre(null, explosion)) {
-            ci.cancel();
+            cir.setReturnValue(0);
         }
     }
 }
